8eebc924d03715d6d843250a70f5f77c0cd1c88c
k-holy <k.holy74@gmail.com>
2013-09-06 16:16:54 +0900

    first commit

---
+++ Tests/ConfigurationTest.php
@@ -0,0 +1,377 @@
+<?php
+/**
+ * Volcanus libraries for PHP
+ *
+ * @copyright 2011-2013 k-holy <k.holy74@gmail.com>
+ * @license The MIT License (MIT)
+ */
+
+namespace Volcanus\Configuration\Tests;
+
+use Volcanus\Configuration\Configuration;
+
+/**
+ * Test for Configuration
+ *
+ * @author k.holy74@gmail.com
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testConstructor()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+			'bar' => false,
+		));
+		$this->assertTrue($config['foo']);
+		$this->assertFalse($config['bar']);
+	}
+
+	public function testConstructorAcceptTraversable()
+	{
+		$config = new Configuration(new \ArrayIterator(array(
+			'foo' => true,
+			'bar' => false,
+		)));
+		$this->assertTrue($config['foo']);
+		$this->assertFalse($config['bar']);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseExceptionWhenAttributeAlreadyDefinedAsAMethod()
+	{
+		$config = new Configuration(array(
+			'offsetSet' => false,
+		));
+	}
+
+	public function testDefineAttribute()
+	{
+		$config = new Configuration();
+		$config->define('foo');
+		$this->assertNull($config['foo']);
+	}
+
+	public function testDefineAttributeWithDefaultValue()
+	{
+		$config = new Configuration();
+		$config->define('foo', true);
+		$this->assertTrue($config['foo']);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseExceptionWhenDefineAttributeAlreadyExists()
+	{
+		$config = new Configuration(array(
+			'foo' => null,
+		));
+		$config->define('foo');
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseExceptionWhenDefineAttributeAlreadyDefinedAsAMethod()
+	{
+		$config = new Configuration();
+		$config->define('offsetGet', true);
+	}
+
+	public function testOffsetSet()
+	{
+		$config = new Configuration(array(
+			'foo' => null,
+		));
+		$config->offsetSet('foo', true);
+		$this->assertTrue($config['foo']);
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseExceptionWhenSetAttributeNotDefined()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$config->offsetSet('bar', 'A');
+	}
+
+	public function testOffsetGet()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$this->assertTrue($config->offsetGet('foo'));
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testRaiseExceptionWhenGetAttributeNotDefined()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$config->offsetGet('bar');
+	}
+
+	public function testOffsetGetCallableAttribute()
+	{
+		$config = new Configuration(array(
+			'foo' => function() {
+				return 'Im Closure';
+			},
+		));
+		$this->assertTrue(is_callable($config->offsetGet('foo')));
+		$this->assertEquals('Im Closure', call_user_func($config->offsetGet('foo')));
+	}
+
+	public function testOffsetGetExecuteCallableAttribute()
+	{
+		$config = new Configuration(array(
+			'foo' => 0,
+			'bar' => function($config) {
+				return $config['foo'] * 2;
+			},
+		), Configuration::EXECUTE_CALLABLE);
+		$this->assertEquals(0, $config->offsetGet('bar'));
+		$config['foo'] = 1;
+		$this->assertEquals(2, $config->offsetGet('bar'));
+		$config['foo'] = 5;
+		$this->assertEquals(10, $config->offsetGet('bar'));
+	}
+
+	public function testCallMethodCallableAttribute()
+	{
+		$config = new Configuration(array(
+			'foo' => function($name) {
+				return sprintf('Im %s', $name);
+			},
+		));
+		$this->assertEquals('Im Argument', $config->foo('Argument'));
+	}
+
+	/**
+	 * @expectedException \BadMethodCallException
+	 */
+	public function testRaiseExceptionWhenCallMethodAttributeNotCallable()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$config->foo();
+	}
+
+	/**
+	 * @expectedException \BadMethodCallException
+	 */
+	public function testRaiseExceptionWhenCallMethodAttributeNotDefined()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$config->bar();
+	}
+
+	public function testToString()
+	{
+		$config = new Configuration(new \ArrayIterator(array(
+			'foo' => true,
+			'bar' => false,
+		)));
+		$this->assertTrue(is_string($config->__toString()));
+	}
+
+	public function testToStringCallableAttribute()
+	{
+		$config = new Configuration(new \ArrayIterator(array(
+			'foo' => true,
+			'bar' => function() {
+				return false;
+			},
+		)));
+		$this->assertTrue(is_string($config->__toString()));
+	}
+
+	public function testOffsetUnset()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+		));
+		$config->offsetUnset('foo');
+		$this->assertNull($config['foo']);
+	}
+
+	public function testOffsetExists()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+			'bar' => null,
+		));
+		$this->assertTrue($config->offsetExists('foo'));
+		$this->assertFalse($config->offsetExists('bar'));
+		$this->assertFalse($config->offsetExists('baz'));
+	}
+
+	public function testArrayAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => null,
+			'bar' => null,
+			'baz' => false,
+		));
+		$config['foo'] = true;
+		$config['bar'] = false;
+		unset($config['baz']);
+		$this->assertTrue($config['foo']);
+		$this->assertFalse($config['bar']);
+		$this->assertNull($config['baz']);
+	}
+
+	public function testPropertyAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => null,
+			'bar' => null,
+		));
+		$config->foo = true;
+		$config->bar = false;
+		$this->assertTrue($config->foo);
+		$this->assertFalse($config->bar);
+	}
+
+	public function testCouldNotUnsetWhenPropertyAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => false,
+		));
+		unset($config->foo);
+		$this->assertFalse($config->foo); // caution!!
+	}
+
+	public function testIssetArrayAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => false,
+			'bar' => null,
+		));
+		$this->assertTrue(isset($config['foo']));
+		$this->assertFalse(isset($config['bar']));
+		$this->assertFalse(isset($config['baz']));
+	}
+
+	public function testIssetPropertyAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => false,
+			'bar' => null,
+		));
+		$this->assertFalse(isset($config->foo)); // WTF!!??
+		$this->assertFalse(isset($config->bar));
+		$this->assertFalse(isset($config->baz));
+	}
+
+	public function testIsNullArrayAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => false,
+			'bar' => null,
+		));
+		$this->assertFalse(is_null($config['foo']));
+		$this->assertTrue(is_null($config['bar']));
+	}
+
+	public function testIsNullPropertyAccess()
+	{
+		$config = new Configuration(array(
+			'foo' => false,
+			'bar' => null,
+		));
+		$this->assertFalse(is_null($config->foo)); // OK
+		$this->assertTrue(is_null($config->bar));
+	}
+
+	public function testRecursiveArrayAccess()
+	{
+		$config = new Configuration(array(
+			'array' => array('a' => 'A', 'b' => 'B', 'c' => 'C'),
+			'object' => new \ArrayObject(array(
+				'a' => new \ArrayObject(array(
+					'a' => array('a' => 'A', 'b' => 'B', 'c' => array('a' => 'A', 'b' => 'B', 'c'=> 'C')),
+					'callable' => function() {
+						return 'Im Closure';
+					},
+				)),
+			)),
+		), Configuration::EXECUTE_CALLABLE);
+		$this->assertEquals('A', $config['array']['a']);
+		$this->assertEquals('B', $config['array']['b']);
+		$this->assertEquals('C', $config['array']['c']);
+		$this->assertEquals('A', $config['object']['a']['a']['a']);
+		$this->assertEquals('B', $config['object']['a']['a']['b']);
+		$this->assertEquals('A', $config['object']['a']['a']['c']['a']);
+		$this->assertEquals('B', $config['object']['a']['a']['c']['b']);
+		$this->assertEquals('C', $config['object']['a']['a']['c']['c']);
+		$this->assertEquals('Im Closure', $config['object']['a']['callable']);
+	}
+
+	public function testRecursivePropertyAccess()
+	{
+		$config = new Configuration(array(
+			'array' => array('a' => 'A', 'b' => 'B', 'c' => 'C'),
+			'object' => new \ArrayObject(array(
+				'a' => new \ArrayObject(array(
+					'a' => array('a' => 'A', 'b' => 'B', 'c' => array('a' => 'A', 'b' => 'B', 'c'=> 'C')),
+					'callable' => function() {
+						return 'Im Closure';
+					},
+				)),
+			)),
+		), Configuration::EXECUTE_CALLABLE);
+		$this->assertEquals('A', $config->array->a);
+		$this->assertEquals('B', $config->array->b);
+		$this->assertEquals('C', $config->array->c);
+		$this->assertEquals('A', $config->object->a->a->a);
+		$this->assertEquals('B', $config->object->a->a->b);
+		$this->assertEquals('A', $config->object->a->a->c->a);
+		$this->assertEquals('B', $config->object->a->a->c->b);
+		$this->assertEquals('C', $config->object->a->a->c->c);
+		$this->assertEquals('Im Closure', $config->object->a->callable);
+	}
+
+	public function testImplementsTraversable()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+			'bar' => false,
+		));
+		$this->assertInstanceOf('\Traversable', $config);
+		foreach ($config as $name => $value) {
+			switch ($name) {
+			case 'foo':
+				$this->assertTrue($value);
+				break;
+			case 'bar':
+				$this->assertFalse($value);
+				break;
+			}
+		}
+	}
+
+	public function testImplementsCountable()
+	{
+		$config = new Configuration(array(
+			'foo' => true,
+			'bar' => false,
+		));
+		$this->assertInstanceOf('\Countable', $config);
+		$this->assertCount(2, $config);
+	}
+
+}
